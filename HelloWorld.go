72fa3453b3dce9d4c2237f84bb43c0a0f8a69169
Won Seong-Yeon <Koreagamemaker@gmail.com>
2020-12-01 23:58:54 +0900

    오 끔찍한 Go세상 2일차

--- HelloWorld.go
+++ HelloWorld.go
@@ -58,6 +58,7 @@ var (
 )
 
 //// 상수의 선언 =================================================================
+//// Go에는 Enum이 존재하지 않고, 상수로 이를 모두 처리한다.
 const (
 	cb   bool    = false // 상수는 zeroValue를 허락하지 않는다
 	ci8  int8    = 1
@@ -65,7 +66,22 @@ const (
 	cs   string  = "ABCD" // 스트링도 상수가 된다;;
 )
 
+//// 구조체의 선언 ================================================================
+type Position struct {
+	X int
+	Y int
+}
+
+//// 타입의 선언 ==================================================================
+type Nickname string
+type Level uint16
+
+// C++ 과 다른 점은 , using 혹은 typedef로 정의할 경우, 단순히 동등 - 치환으로 생각하는데,
+// Golang의 Type은 말 그대로 다른 Type으로 서로 동등하지 않다.
+
 func main() {
+
+	fmt.Println()
 
 	// 기본 출력문은 다음과 같이 처리한다. =================================================================
 	{
@@ -73,14 +89,78 @@ func main() {
 		fmt.Println("The time is", time.Now())
 	}
 
+	fmt.Println()
+
+	// 배열의 선언은 다음과 같이 처리합니다.
+	{
+		var temp [2]int
+		temp[0] = 100
+		temp[1] = 150
+		fmt.Println("temp:", temp)
+
+		var temp2 = [3]string{"ABC", "XYZ", "WSY"}
+		fmt.Println("temp:", temp2)
+
+		temp2ptr := &temp2[2]
+		*temp2ptr = "NEO"
+		fmt.Println("temp:", temp2)
+	}
+
+	fmt.Println()
+
+	// C++ 에는 없는 슬라이스...? 가변 길이의 배열이라는데 벡터인 것인가 리스트인 것인가는 뭔가 배열의 레퍼런스인가...?
+	{
+		// 기존의 배열을 레퍼런스 화한다.
+		{
+			temp := [3]int{10, 20, 30}
+			tempSlice := temp[0:3]
+			// 0번쨰 인덱스부터 3보다 작은 인덱스 2까지로 슬라이스를 만든다.
+			// temp [ 0:3] == temp [ 0:] == temp [:3] == temp[:]
+
+			fmt.Println("tempSlice : ", tempSlice)
+			tempSlice[0] = 100
+
+			fmt.Println("tempSlice change 'temp' : ", temp)
+			tempSliceSlice := tempSlice[2:3]
+			tempSliceSlice[0] = 1000
+
+			fmt.Println("tempSliceSlice change 'temp' : ", temp)
+		}
+
+		// 슬라이스 자체를 바로 만든다.
+		{
+			tempSlice := []int{1, 2, 3, 4, 5}
+			fmt.Println("tempSlice: ", tempSlice)
+
+			// 슬라이스를 만드는데 세상에 구조체를 바로만들어서 슬라이스를 만든다...?
+			tempStructSlice := []struct {
+				name string
+				age  uint8
+			}{
+				{"", 1},
+				{"", 2}
+			}
+		}
+	}
+
+	fmt.Println()
+
 	// for문은 다음과 같이 합니다. (엥 괄호가 없네) ( go에는 while문을 지원하지 않습니다. ) =====================
 	{
 		for i := 0; i < 3; i++ {
 			fmt.Println("for Loop : ", i)
 		}
+
+		//범위 기반 for문은 다음과 같이 돌립니다.
+		var temp = [2]int{10, 20}
+		for i := range temp {
+			fmt.Println("for Loop Range : ", temp[i])
+		}
 	}
 
-	// if문은 다음과 같이 합니다. 여기도 괄호가 없지만, C++17처럼 If-Init이 가능함
+	fmt.Println()
+
+	// if문은 다음과 같이 합니다. 여기도 괄호가 없지만, C++17처럼 If-Init이 가능함 =====================
 	{
 		if var1 := 1; var1 == 0 {
 			fmt.Println("if var == 0 : ", var1)
@@ -89,5 +169,66 @@ func main() {
 		}
 	}
 
-	// switch문은 기존과 다르게 fallthrough
+	fmt.Println()
+
+	// switch문은 기존과 다르게 별도의 break없이도 멈추며, fallthrough를 명시하지 않으면 해당 Case만 실행됩니다.
+	{
+		temp := 0
+		switch temp {
+		case 0:
+			temp++
+		case 1:
+			temp++
+		case 2:
+			temp++
+		}
+		fmt.Println("switch not fallthrough : ", temp)
+
+		temp = 0
+
+		switch temp {
+		case 0:
+			temp++
+			fallthrough
+		case 1:
+			temp++
+			fallthrough
+		case 2:
+			temp++
+			fallthrough
+		default:
+		}
+		fmt.Println("swith with fallthrough : ", temp)
+	}
+
+	fmt.Println()
+
+	// omg! golang pointer! ====================================================================
+	{
+		temp1 := 10
+		temp2 := 20
+
+		// pointer var
+		pTemp := &temp1
+
+		fmt.Println("ptemp :", pTemp, ", &pTemp :", &pTemp, ", *pTemp :", *pTemp)
+		fmt.Println("temp1 :", temp1, ", &temp1 :", &temp1)
+
+		//  change value to p-Var
+		*pTemp = 100
+
+		fmt.Println("ptemp :", pTemp, ", &pTemp :", &pTemp, ", *pTemp :", *pTemp)
+		fmt.Println("temp1 :", temp1, ", &temp1 :", &temp1)
+
+		//
+		pTemp = &temp2
+
+		fmt.Println("ptemp :", pTemp, ", &pTemp :", &pTemp, ", *pTemp :", *pTemp)
+		fmt.Println("temp2 :", temp2, ", &temp2 :", &temp2)
+	}
+
+	//
+	{
+
+	}
 }
