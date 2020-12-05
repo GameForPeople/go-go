1ab1ceea44bf41d25b2cb936915e270fb926d133
Won Seong-Yeon <Koreagamemaker@gmail.com>
2020-12-05 17:13:41 +0900

    golang echo server Go언어 못쓰것다

--- echoServer/main.go
+++ echoServer/main.go
@@ -1,7 +1,75 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strconv"
+)
+
+const (
+	PORT_NUMBER int = 7777
+	LOCAL_HOST string = "127.0.0.1:"
+)
 
 func main() {
-	fmt.Println("Hello Go!")
+	portString := strconv.Itoa(PORT_NUMBER)
+
+	// 서버를 Listen 상태로 변경합니다.
+	listener, error := net.Listen("tcp", LOCAL_HOST+portString)
+	if listener == nil {
+		panic("Failed to listen:" + error.Error())
+	}
+
+	fmt.Println("Listening Ip:", LOCAL_HOST)
+	fmt.Println("Listening Port:", portString)
+
+	clients := AcceptClient(listener)
+	for {
+		go HandleClient(<-clients)
+	}
+}
+
+// Accept합니다.
+func AcceptClient(listner net.Listener) chan net.Conn {
+	ch := make(chan net.Conn)
+
+	go func() {
+		for {
+			client, error := listner.Accept()
+			if client == nil {
+				fmt.Println("failed to accept:", error.Error())
+			}
+
+			fmt.Printf("new connection: %v <-> %v\n", client.LocalAddr(), client.RemoteAddr())
+			ch <- client
+		}
+	}()
+
+	return ch
+}
+
+// 
+func HandleClient(client net.Conn) {
+	defer fmt.Println("close client")
+	defer client.Close()
+
+	fmt.Println("Connected from:", client.RemoteAddr().String())
+
+	reader := bufio.NewReader(client)
+	for {
+		line, error := reader.ReadBytes('\n')
+		if error != nil {
+			fmt.Println("failed to read from client.", error.Error())
+			break
+		}
+
+		fmt.Println(string(line))
+
+		_, error = client.Write(line)
+		if error != nil {
+			fmt.Println("write error:", error)
+			return
+		}
+	}
 }
